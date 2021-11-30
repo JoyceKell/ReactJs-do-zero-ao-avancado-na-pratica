fb30f52859e08594d1874dfb0805eaa3b0f0444b
Joyce Kelly <joyccekellywork@gmail.com>
2021-11-30 11:12:42 -0300

    trabalhando com loops

---
+++ evoluindo_no_javascript_ES6+/script.js
@@ -0,0 +1,13 @@
+//while
+
+//x = 0
+//while(x<10){
+// document.write("</br> O valor do x e: " + x);
+// x++;
+//}
+
+//for
+
+for(a = 0; a<10; a++){
+  document.write("</br> O valor do a e: " + a);
+}
