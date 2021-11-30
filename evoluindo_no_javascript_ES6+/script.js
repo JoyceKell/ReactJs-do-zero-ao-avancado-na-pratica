f700fd34d45dc431b75153b8e348b7a46fd7a1b3
Joyce Kelly <joyccekellywork@gmail.com>
2021-11-30 15:03:22 -0300

    operacoes em array

--- evoluindo_no_javascript_ES6+/script.js
+++ evoluindo_no_javascript_ES6+/script.js
@@ -1,4 +1,17 @@
-function minhaLista(...nomes){
-  console.log(nomes)
-}
-minhaLista("Matheus", "Lucas", "João")
+const lista = [1,2,3,4,5,6];
+
+const novalista = lista.map(function(item){
+  return item*2;
+})
+
+console.log(novalista)
+
+const soma = lista.reduce(function(total,proximo){
+  return total+proximo;
+})
+
+const find = lista.find(function(item){
+  return item===6;
+})
+console.log(find)
+console.log(soma)
