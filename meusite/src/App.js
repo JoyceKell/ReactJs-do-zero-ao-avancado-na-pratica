6138a7817915ca78154f4be96b90268aa004d88c
Joyce Kelly <joyccekellywork@gmail.com>
2021-12-01 11:43:05 -0300

    aplicando class component

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,30 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Sobre = (props)=>{
-  return (
-    <div>
-      <h2>Olá, sou o {props.nome}</h2>
-      <h3>cargo: {props.cargo}</h3>
-      <h3>idade: {props.idade} </h3>
-    </div>
-  );
-}
-
-const Equipe = (props) => {
-  return(
-    <div>
-      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
-    </div>
-  )
+class Equipe extends Component{
+  render(){
+    return(
+      <div>
+        <h2>Funcionou</h2>
+        <h2>{this.props.nome}</h2>
+      </div>
+    );
+  }
 }
 
 function App(){
   return(
     <div>
-      <h1>conheça a nossa equipe: </h1>
-      <Equipe nome="Joyce" cargo="programadora" idade="21"/>
+      <h1>conheça a nossa equipe</h1>
+      <Equipe nome="joyce"/>
     </div>
-  );
+  )
 }
 
 export default App;
