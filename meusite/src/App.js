a3bda594d568948addd2cf9376d16f61b399b1d5
Joyce Kelly <joyccekellywork@gmail.com>
2021-12-01 11:55:42 -0300

    trabalhando com states

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,23 +1,36 @@
 import React, {Component} from 'react';
 
-class Equipe extends Component{
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nome:"matheus",
+      contador:0
+    }
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+  aumentar(){
+    let state = this.state;
+    state.contador +=1; 
+    this.setState(state);
+  }
+
+  diminuir(){
+    let state = this.state;
+    state.contador -=1;
+    this.setState(state);
+  }
+
   render(){
     return(
       <div>
-        <h2>Funcionou</h2>
-        <h2>{this.props.nome}</h2>
+        <h1>conheça a nossa equipe</h1>
+        <h3><button onClick={this.aumentar}>aumentar</button>{this.state.contador} <button onClick={this.diminuir}>diminuir</button></h3>
       </div>
-    );
+    )
   }
-}
-
-function App(){
-  return(
-    <div>
-      <h1>conheça a nossa equipe</h1>
-      <Equipe nome="joyce"/>
-    </div>
-  )
 }
 
 export default App;
