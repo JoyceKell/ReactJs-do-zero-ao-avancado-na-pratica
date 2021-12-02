99d252de85bbad908f9c4e1ac6adee92369435bf
Joyce Kelly <joyccekellywork@gmail.com>
2021-12-02 10:18:39 -0300

    trabalhando com listas

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
-import Membro from './components/membro';
+import Feed from './components/feed';
 
 class App extends Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+        feed:[
+          {id:1, username: "Matheus", curtidas:10, comentarios:2},
+          {id:2, username: "Lucas", curtidas:100, comentarios:17}
+        ]
+    }
+  }
 
   render(){
     return(
       <div> 
-       <Membro nome="visitante"/>
+        {this.state.feed.map((item)=>{
+          return(
+            <Feed key={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios}/>
+          )
+        })}
+        
       </div>
     )
   }
