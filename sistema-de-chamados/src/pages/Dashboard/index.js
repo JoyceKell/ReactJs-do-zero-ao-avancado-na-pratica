56a4089732c2ab10f30d22a58f8d45844b01ec51
Joyce Kelly <joyccekellywork@gmail.com>
2021-12-16 10:38:17 -0300

    buscando chamados pt2

--- sistema-de-chamados/src/pages/Dashboard/index.js
+++ sistema-de-chamados/src/pages/Dashboard/index.js
@@ -38,6 +38,15 @@ export default function Dashboard(){
     setLoading(false);
   }
 
+  async function handleMore(){
+    setLoadingMore(true);
+    await listRef.startAfter(lastDocs).limit(5).get()
+    .then((snapshot)=>{
+      updateState(snapshot);
+    })
+    .catch((error)=>{})
+  }
+
   async function updateState(snapshot){
     const isCollectionEmpty = snapshot.size === 0;
 
@@ -47,7 +56,7 @@ export default function Dashboard(){
         lista.push({
           id: item.id,
           assunto: item.data().assunto,
-          cliente: item.data().ncliente,
+          cliente: item.data().cliente,
           clienteId: item.data().clienteId,
           created: item.data().created,
           createdFormated: format(item.data().created.toDate(),'dd/MM/yyyy'),
@@ -63,6 +72,23 @@ export default function Dashboard(){
       setIsEmpty(true);
     }
     setLoadingMore(false);
+  }
+
+  if(loading){
+    return(
+      <div>
+         <Header/>
+          <div className="content">
+          <Title name="Atendimentos">
+            <FiMessageSquare size={25}/>
+          </Title>
+
+          <div className="container dashboard">
+            <span>Buscando chamados...</span>
+          </div>
+          </div>
+      </div>
+    )
   }
   
   return(
@@ -91,18 +117,26 @@ export default function Dashboard(){
            </tr>
          </thead>
          <tbody>
-           <tr>
-             <td data-label="Cliente">Sujeito</td>
-             <td data-label="Assunto">Suporte</td>
-             <td data-label="Status"><span className="badge" style={{backgroundColor: '#5cb85c'}}>Em aberto</span></td>
-             <td data-label="Cadastrado">20/06/2021</td>
-             <td data-label="#">
-               <button className="action" style={{backgroundColor: '#3583f6'}}><FiSearch color="#fff" size={17}/></button>
-               <button className="action" style={{backgroundColor: '#f6a935'}}><FiEdit2 color="#fff" size={17}/></button>
-             </td>
-           </tr>
+           {chamados.map((item, index)=>{
+             return(
+                <tr key={index}>
+                <td data-label="Cliente">{item.cliente}</td>
+                <td data-label="Assunto">{item.assunto}</td>
+                <td data-label="Status"><span className="badge" style={{backgroundColor: item.status === 'aberto' ? '#5cb85c' : '#999'}}>{item.status}</span></td>
+                <td data-label="Cadastrado">{item.createdFormated}</td>
+                <td data-label="#">
+                  <button className="action" style={{backgroundColor: '#3583f6'}}><FiSearch color="#fff" size={17}/></button>
+                  <button className="action" style={{backgroundColor: '#f6a935'}}><FiEdit2 color="#fff" size={17}/></button>
+                </td>
+              </tr>
+             )
+           })}
+           
          </tbody>
        </table>
+        
+        {loadingMore && <h3 style={{textAlign:"center",marginTop:15}}>Buscando dados...</h3>}
+        { !loadingMore && !isEmpty && <button className="btn-more" onClick={handleMore}>Buscar Mais</button>}
       </>}
 
       
